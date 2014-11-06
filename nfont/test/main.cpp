454b9613f9005e05c7ead9b76d90003144af5595
grimfang4@gmail.com <grimfang4@gmail.com@40d576c2-85fa-11de-a5a5-a901c6d87a3b>
2014-11-06 16:23:47 +0000

    Updated test program to use the new NFont, SDL2, and SDL_gpu.

--- nfont/test/main.cpp
+++ nfont/test/main.cpp
@@ -4,41 +4,41 @@
 #include <cmath>
 
 
-void drawRect(SDL_Surface* surf, const SDL_Rect& rect, Uint32 color)
+void drawRect(GPU_Target* target, const GPU_Rect& rect, const SDL_Color& color)
 {
-    SDL_Rect r;
+    GPU_Rect r;
     r.x = rect.x;
     r.y = rect.y;
     r.w = rect.w;
     r.h = 1;
-    SDL_FillRect(surf, &r, color);
+    GPU_RectangleFilled(target, r.x, r.y, r.x + r.w - 1, r.y + r.h - 1, color);
     r.y = rect.y + rect.h;
-    SDL_FillRect(surf, &r, color);
+    GPU_RectangleFilled(target, r.x, r.y, r.x + r.w - 1, r.y + r.h - 1, color);
     
     r.y = rect.y;
     r.w = 1;
     r.h = rect.h;
-    SDL_FillRect(surf, &r, color);
+    GPU_RectangleFilled(target, r.x, r.y, r.x + r.w - 1, r.y + r.h - 1, color);
     r.x = rect.x + rect.w;
-    SDL_FillRect(surf, &r, color);
+    GPU_RectangleFilled(target, r.x, r.y, r.x + r.w - 1, r.y + r.h - 1, color);
 }
 
-void loop_drawSomeText(SDL_Surface* screen)
+void loop_drawSomeText(GPU_Target* screen)
 {
     NFont font("../fonts/FreeSans.ttf", 20, NFont::Color(0,0,0,255));
     NFont font2("../fonts/FreeSans.ttf", 18, NFont::Color(0,200,0,255));
     NFont font3("../fonts/FreeSans.ttf", 22, NFont::Color(0,0,200,255));
     
-    SDL_Rect leftHalf = {0,0,3*screen->w/4, screen->h};
-    SDL_Rect rightHalf = {leftHalf.w,0,screen->w/4, screen->h};
+    GPU_Rect leftHalf = {0,0,3*screen->w/4, screen->h};
+    GPU_Rect rightHalf = {leftHalf.w,0,screen->w/4, screen->h};
     
-    SDL_Rect box1 = {215, 50, 150, 150};
-    SDL_Rect box2 = {215, box1.y + box1.h + 50, 150, 150};
-    SDL_Rect box3 = {215, box2.y + box2.h + 50, 150, 150};
+    GPU_Rect box1 = {215, 50, 150, 150};
+    GPU_Rect box2 = {215, box1.y + box1.h + 50, 150, 150};
+    GPU_Rect box3 = {215, box2.y + box2.h + 50, 150, 150};
     
     int scroll = 0;
     
-    Uint8* keystates = SDL_GetKeyState(NULL);
+    Uint8* keystates = SDL_GetKeyboardState(NULL);
     
     bool done = false;
 	SDL_Event event;
@@ -55,13 +55,14 @@ void loop_drawSomeText(SDL_Surface* screen)
 	        }
 	    }
 	    
-	    if(keystates[SDLK_UP])
+	    if(keystates[SDL_SCANCODE_UP])
             scroll--;
-	    else if(keystates[SDLK_DOWN])
+	    else if(keystates[SDL_SCANCODE_DOWN])
             scroll++;
 	    
-	    SDL_FillRect(screen, &leftHalf, 0xffffff);
-	    SDL_FillRect(screen, &rightHalf, 0x777777);
+	    GPU_RectangleFilled(screen, leftHalf.x, leftHalf.y, leftHalf.x + leftHalf.w - 1, leftHalf.y + leftHalf.h - 1, GPU_MakeColor(255, 255, 255, 255));
+	    GPU_RectangleFilled(screen, rightHalf.x, rightHalf.y, rightHalf.x + rightHalf.w - 1, rightHalf.y + rightHalf.h - 1, GPU_MakeColor(0x77, 0x77, 0x77, 255));
+	    
 	    
 	    font.draw(screen, rightHalf.x, 5, NFont::LEFT, "draw align LEFT");
 	    font.draw(screen, rightHalf.x, 25, NFont::CENTER, "draw align CENTER");
@@ -94,25 +95,25 @@ void loop_drawSomeText(SDL_Surface* screen)
         font.drawColumn(screen, 200, 450, 200, NFont::RIGHT, "column align RIGHT\n\nColumn text wraps at the width of the column and has no maximum height.");
 	    
 	    
-	    drawRect(screen, box1, 0x000000);
-	    drawRect(screen, box2, 0x000000);
-	    drawRect(screen, box3, 0x000000);
+	    drawRect(screen, box1, GPU_MakeColor(0, 0, 0, 255));
+	    drawRect(screen, box2, GPU_MakeColor(0, 0, 0, 255));
+	    drawRect(screen, box3, GPU_MakeColor(0, 0, 0, 255));
 	    
-	    SDL_SetClipRect(screen, &box1);
+	    GPU_SetClipRect(screen, box1);
 	    SDL_Rect box1a = {box1.x, box1.y - scroll, box1.w, box1.h + scroll};
         font.drawBox(screen, box1a, "box align LEFT\n\nBox text wraps at the width of the box and is clipped to the maximum height.");
         
-	    SDL_SetClipRect(screen, &box2);
+	    GPU_SetClipRect(screen, box2);
 	    SDL_Rect box2a = {box2.x, box2.y - scroll, box2.w, box2.h + scroll};
         font.drawBox(screen, box2a, NFont::CENTER, "box align CENTER\n\nBox text wraps at the width of the box and is clipped to the maximum height.");
         
-	    SDL_SetClipRect(screen, &box3);
+	    GPU_SetClipRect(screen, box3);
 	    SDL_Rect box3a = {box3.x, box3.y - scroll, box3.w, box3.h + scroll};
         font.drawBox(screen, box3a, NFont::RIGHT, "box align RIGHT\n\nBox text wraps at the width of the box and is clipped to the maximum height.");
         
-        SDL_SetClipRect(screen, NULL);
+	    GPU_UnsetClip(screen);
 	    
-	    SDL_Flip(screen);
+	    GPU_Flip(screen);
 	    
 	    SDL_Delay(1);
 	}
@@ -121,20 +122,13 @@ void loop_drawSomeText(SDL_Surface* screen)
 
 int main(int argc, char* argv[])
 {
-	if(SDL_Init(SDL_INIT_VIDEO ) < 0)
-	{
-        printf("Couldn't initialize SDL: %s\n", SDL_GetError());
-        return 1;
-    }
-    
     int w = 800;
     int h = 600;
-	SDL_Surface* screen = SDL_SetVideoMode(w, h, 0, SDL_SWSURFACE);
-	
-    if(screen == NULL)
-    {
-        printf("Couldn't set video mode %dx%d: %s\n", w, h, SDL_GetError());
-		return 1;
+    GPU_Target* screen = GPU_Init(w, h, GPU_DEFAULT_INIT_FLAGS);
+	if(screen == NULL)
+	{
+        GPU_LogError("Couldn't initialize SDL_gpu\n");
+        return 1;
     }
     
 	loop_drawSomeText(screen);
